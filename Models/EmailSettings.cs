769eb37568dc607a09099161a036ba6edb72e8fd
Jose David Millón <josed200096@gmail.com>
2025-11-26 13:32:43 -0600

    email

--- Models/EmailSettings.cs
+++ Models/EmailSettings.cs
@@ -2,13 +2,11 @@
 {
     public class EmailSettings
     {
-
-        public string Host => Environment.GetEnvironmentVariable("SMTP_HOST");
-        public int Port => int.Parse(Environment.GetEnvironmentVariable("SMTP_PORT") ?? "587");
-        public string User => Environment.GetEnvironmentVariable("SMTP_USER");
-        public string Password => Environment.GetEnvironmentVariable("SMTP_PASS");
-
-
+        public string Host { get; set; } = Environment.GetEnvironmentVariable("SMTP_HOST");
+        public int Port { get; set; } = int.TryParse(Environment.GetEnvironmentVariable("SMTP_PORT"), out var p) ? p : 587;
+        public string User { get; set; } = Environment.GetEnvironmentVariable("SMTP_USER");
+        public string Password { get; set; } = Environment.GetEnvironmentVariable("SMTP_PASS");
     }
+
 
 }
